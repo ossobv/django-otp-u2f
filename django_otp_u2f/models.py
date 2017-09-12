ee166cbcfb3b4053dc8057e3818a1d43b53469ca
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2017-09-12 09:11:51 +0100

    Refactor to support multiple U2F security keys off one django-otp Device

--- django_otp_u2f/models.py
+++ django_otp_u2f/models.py
@@ -1,14 +1,12 @@
 import json
 
-import copy
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.serialization import Encoding
+from cryptography.x509 import NameOID
 from django.conf import settings
 from django.db import models
 from django_otp.models import Device
 
-from u2flib_server import u2f
 from u2flib_server.model import DeviceRegistration
 from u2flib_server.u2f import (begin_registration, begin_authentication,
                                complete_registration, complete_authentication)
@@ -16,54 +14,31 @@ from u2flib_server.utils import websafe_decode
 
 
 class U2FDevice(Device):
-    version = models.CharField(max_length=16, blank=True)
-    public_key = models.TextField(blank=True)
-    transports = models.TextField(default='[]', blank=True)
-    enroll_challenge = models.TextField(blank=True)
-    key_handle = models.TextField(blank=True)
+    @staticmethod
+    def begin_registration():
+        """
+        Start registering a U2F security key.
 
-    def as_device_registration(self):
-        return DeviceRegistration(version=self.version,
-                                  keyHandle=self.key_handle,
-                                  appId=settings.OTP_U2F_APP_ID,
-                                  publicKey=self.public_key,
-                                  transports=json.loads(self.transports))
+        Keep enroll_challenge safe server-side, and give data_for_client to the client. When you get the data back from
+        the client, call U2FDevice.create_key with it.
 
-    @classmethod
-    def enroll(cls, user):
+        :return: enroll_challenge, data_for_client
+        """
         enroll = begin_registration(settings.OTP_U2F_APP_ID, [])
-        device = cls.objects.create(user=user, confirmed=False, enroll_challenge=json.dumps(enroll.json))
-        data_for_client = copy.deepcopy(enroll.data_for_client)
-        return device, data_for_client
+        return enroll.json, enroll.data_for_client
 
-    def bind(self, data):
-        data['version'] = 'U2F_V2'
-        device, cert = complete_registration(json.loads(self.enroll_challenge), data, [settings.OTP_U2F_APP_ID])
-        self.version = device['version']
-        self.public_key = device['publicKey']
-        self.transports = json.dumps(device['transports'])
-        self.key_handle = device['keyHandle']
-
-        cert = x509.load_der_x509_certificate(cert, default_backend())
-        # log.debug("Attestation certificate:\n%s",
-        #           cert.public_bytes(Encoding.PEM))
-        self.enroll_challenge = ''
-        self.save()
+    def create_key(self, enroll_challenge, data):
+        u2f_key = U2FKey.create(self, enroll_challenge, data)
+        self.confirmed = True
+        return u2f_key
 
     def generate_challenge(self):
         challenge = begin_authentication(settings.OTP_U2F_APP_ID,
-                                         [self.as_device_registration()])
-        u2f_challenge = U2FChallenge.objects.create(device=self,
-                                                    challenge=challenge.data_for_client['challenge'],
-                                                    challenge_data=challenge.json)
-        data_for_client = copy.copy(challenge.data_for_client)
-        return {
-            'version': data_for_client['registeredKeys'][0]['version'],
-            'challenge': data_for_client['challenge'],
-            'appId': data_for_client['registeredKeys'][0]['appId'],
-            'keyHandle': data_for_client['registeredKeys'][0]['keyHandle'],
-            'requestId': u2f_challenge.id,
-        }
+                                         [key.as_device_registration() for key in self.u2fkey_set.all()])
+        U2FChallenge.objects.create(device=self,
+                                    challenge=challenge.data_for_client['challenge'],
+                                    challenge_data=challenge.json)
+        return challenge.data_for_client
 
     def verify_token(self, token):
         client_data = json.loads(websafe_decode(token['clientData']).decode())
@@ -78,8 +53,51 @@ class U2FDevice(Device):
         u2f_challenge.delete()
         return True
 
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        self.confirmed = self.u2fkey_set.exists() if self.pk else False
+        super(U2FDevice, self).save(force_insert, force_update, using, update_fields)
+
+
+class U2FKey(models.Model):
+    device = models.ForeignKey(U2FDevice, on_delete=models.CASCADE)
+    name = models.TextField()
+    version = models.CharField(max_length=16, blank=True)
+    public_key = models.TextField(blank=True)
+    transports = models.TextField(default='[]', blank=True)
+    enroll_challenge = models.TextField(blank=True)
+    key_handle = models.TextField(blank=True)
+
+    def as_device_registration(self):
+        return DeviceRegistration(version=self.version,
+                                  keyHandle=self.key_handle,
+                                  appId=settings.OTP_U2F_APP_ID,
+                                  publicKey=self.public_key,
+                                  transports=json.loads(self.transports))
+
+    @classmethod
+    def create(cls, u2f_device, enroll_challenge, data, name=None):
+        data['version'] = 'U2F_V2'
+        device, cert = complete_registration(json.loads(enroll_challenge), data, [settings.OTP_U2F_APP_ID])
+        if not name:
+            cert = x509.load_der_x509_certificate(cert, default_backend())
+            name = cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
+
+        return cls.objects.create(name=name,
+                                  device=u2f_device,
+                                  version=device['version'],
+                                  public_key=device['publicKey'],
+                                  transports=json.dumps(device['transports']),
+                                  key_handle=device['keyHandle'])
+
+    def delete(self, using=None, keep_parents=False):
+        super(U2FKey, self).delete(using, keep_parents)
+        self.device.save()
+
 
 class U2FChallenge(models.Model):
     device = models.ForeignKey(U2FDevice)
     challenge = models.TextField(db_index=True)
     challenge_data = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
--- django_otp_u2f/tests/test_device.py
+++ django_otp_u2f/tests/test_device.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+import u2flib_host.authenticate
 import u2flib_host.soft
 import u2flib_host.u2f
 
@@ -13,29 +14,44 @@ from django_otp_u2f import models
 
 
 class U2FDeviceTestCase(TestCase):
+    def sign_challenge(self, devices, challenge):
+        for registered_key in challenge['registeredKeys']:
+            key_challenge = {
+                'appId': registered_key['appId'],
+                'keyHandle': registered_key['keyHandle'],
+                'version': registered_key['version'],
+                'challenge': challenge['challenge'],
+            }
+            return u2flib_host.authenticate.authenticate(devices, key_challenge, settings.OTP_U2F_APP_ID, False)
+
     def test_enroll(self):
         user = get_user_model().objects.create()
-        device, data_for_client = models.U2FDevice.enroll(user=user)
-        self.assertIsInstance(device, models.U2FDevice)
+        u2f_device = models.U2FDevice.objects.create(user=user)
+        self.assertFalse(u2f_device.confirmed)
+
+        enroll_challenge, data_for_client = u2f_device.begin_registration()
 
         filename = tempfile.mktemp()
         try:
             soft_device = u2flib_host.soft.SoftU2FDevice(filename)
             result = u2flib_host.u2f.register(soft_device, data_for_client['registerRequests'][0],
                                               settings.OTP_U2F_APP_ID)
-            device.bind(result)
+            u2f_device.create_key(enroll_challenge, result)
+            self.assertTrue(u2f_device.confirmed)
 
-            challenge = device.generate_challenge()
-            result = u2flib_host.u2f.authenticate(soft_device, challenge, settings.OTP_U2F_APP_ID)
-            self.assertTrue(device.verify_token(result))
+            challenge = u2f_device.generate_challenge()
+            result = self.sign_challenge([soft_device], challenge)
+            self.assertTrue(u2f_device.verify_token(result))
 
             # Generate another challenge
-            challenge = device.generate_challenge()
-            result = u2flib_host.u2f.authenticate(soft_device, challenge, settings.OTP_U2F_APP_ID)
-            self.assertTrue(device.verify_token(result))
+            challenge = u2f_device.generate_challenge()
+            result = self.sign_challenge([soft_device], challenge)
+            self.assertTrue(u2f_device.verify_token(result))
             # Can't verify the same token twice
-            self.assertFalse(device.verify_token(result))
+            self.assertFalse(u2f_device.verify_token(result))
 
+            u2f_device.u2fkey_set.get().delete()
+            self.assertFalse(u2f_device.confirmed)
         finally:
             if os.path.exists(filename):
                 os.unlink(filename)
