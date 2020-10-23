5b06e4c7d47e353c2ba02b411ac7a29a9ed275b8
Harm Geerts <hgeerts@osso.nl>
2020-10-23 15:18:00 +0200

    Bump version: 0.1.4 → 0.1.5

--- otp_u2f/__init__.py
+++ otp_u2f/__init__.py
@@ -6,6 +6,6 @@ Django OTP U2F plugin for Django OTP and Kleides Multi Factor Authentication.
 
 __author__ = """Harm Geerts"""
 __email__ = 'hgeerts@osso.nl'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 default_app_config = 'otp_u2f.apps.OtpU2fConfig'
